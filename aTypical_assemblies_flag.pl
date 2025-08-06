f29ebde40a8bc6df6f3b8bceb391d71880201e2f
Caitlin <118347463+cake-tin99@users.noreply.github.com>
2025-08-06 14:00:36 +0100

    Add files via upload

---
+++ aTypical_assemblies_flag.pl
@@ -0,0 +1,16 @@
+#!/bin/bash
+counter=0
+while read line;
+do
+counter=$((counter+1))
+test=$(echo $line | sed 's|\(.*\)_.*|\1|' );
+wget -q https://api.ncbi.nlm.nih.gov/datasets/v1/genome/accession/${test}/download
+unzip -q download
+isAtypical=$(grep -c isAtypical ./ncbi_dataset/data/assembly_data_report.jsonl)
+echo $line $isAtypical >> scraping_atypical_assemblies.txt
+rm download 
+rm -r ncbi_dataset
+rm README.md
+echo $counter
+done < curated_database
+#done
